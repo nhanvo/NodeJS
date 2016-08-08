4568ae86e1dbc5787d0ef53ab4cf77792cf48eb0
NhanVo <vothanhnhan.it@gmail.com>
2016-08-08 13:32:06 +0700

    Update server file

--- scotch/server.js
+++ scotch/server.js
@@ -6,11 +6,12 @@ var mongoose  = require('mongoose');    // mongoose for mongodb
 var morgan    = require('morgan');      // log request to the console (express 4)
 var bodyParser= require('body-parser'); // pull information from HTML POST (express 4)
 var methodOverride = require('method-override');    // simulate DELETE and PUT (express 4)
+var database = require('./config/database');
 
 /*========================configuration========================*/
 
 // connect to mongoDB database on modulus.io
-mongoose.connect('mongodb://nhanvo:daihaithuy512@jello.modulusmongo.net:27017/axYvos3e')
+mongoose.connect(database.url);
 
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public'));
@@ -28,66 +29,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
 app.use(methodOverride());
+app.set('port', process.env.PORT || 3000);
 
-/*========================model========================*/
-var Todo = mongoose.model('Todo', {
-    text: String
-})
+/*========================routes=============================*/
+require('./app/routes')(app);
 
-/*========================routes========================*/
-
-// Get all todos
-app.get('/api/todos', function(req, res) {
-    // use mongoose to get all todos in the database
-    Todo.find(function(err, todos){
-        if (err)
-            res.send(err);
-        console.log(todos);
-        // return all todos in JSON format
-        res.json(todos); 
-    });
+/*========================application========================*/
+app.get('*', function(req, res) {
+    res.sendfile('.public/index.html');
 });
-
-// Create todo and send back  all todos after creation
-app.post('/api/todos', function(req, res) {
-    // create one todo, information comes from AJAX request from AngularJS
-    Todo.create({
-        text: req.body.text,
-        done: false
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos){
-            if (err)
-                res.send(err);
-
-            // return all todos in JSON format
-            res.json(todos);
-        });
-    });
-});
-
-// delete a todo
-app.delete('/api/todos/:todo_id', function(req, res) {
-    Todo.remove({
-        _id: req.params.todo_id
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err)
-
-            res.json(todos);
-        });
-    })
-});
-
 
 /*========================listen (start app with node server.js)*/
-app.listen(8080);
-console.log("app listening on port 8080");
+app.listen(app.get('port'), () => {
+    console.log('listening on ' + app.get('port'));
+});
